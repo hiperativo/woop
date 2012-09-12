923d0a46de63b20f6fd5fe71a9d821c917d6f8aa
Pedro Maciel <pedrozath@gmail.com>
2012-09-12 15:36:00 -0300

    reply to

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ActionMailer::Base
   default :from => "#{ENV['EMAIL_LOGIN']}@#{ENV['EMAIL_DOMAIN']}"
   def contact_mail(usuario)
   	@usuario = usuario
-  	mail :to => ENV['EMAIL_RECEIVER'], :bcc => 'pedrozath@gmail.com', :subject => "Contato via Site Woop: #{@usuario.subject}", :reply_to => "contato@woop.com.br"
+  	mail :to => ENV['EMAIL_RECEIVER'], :bcc => 'pedrozath@gmail.com', :subject => "Contato via Site Woop: #{@usuario.subject}", :reply_to => "atendimento@woop.com.br"
   end
 end
